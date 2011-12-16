6db0fa0593327eccc3c017c378b222298230dc45
Maciej Pacula <maciej.pacula@gmail.com>
2011-12-15 21:33:45 -0500

    path fix in run-benchmark.sh

--- run-bechmark.sh
+++ run-bechmark.sh
@@ -11,7 +11,7 @@ for program in colocations-cpp colocations-hs; do
         echo "  n=" $n
         for sample in $( seq 1 $samples ); do
             echo "    sample="$sample
-            time=$( head -n $n input.txt | /usr/bin/time -f "%e    %M" "$basedir/$program" 2>&1 1>/dev/null )
+            time=$( head -n $n "$basedir/data/input.txt" | /usr/bin/time -f "%e    %M" "$basedir/$program" 2>&1 1>/dev/null )
             printf "%-20s%-20s%-20s\n" $program $n "$time" >> "$basedir/data/$program-times.csv"
         done
     done
