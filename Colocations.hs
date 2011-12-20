7cdb4c4ffb14b01b0cdba110bc933091c06d373f
Bas van Dijk <v.dijk.bas@gmail.com>
2011-12-20 16:55:53 +0100

    Use Text Builders which improves performance by 15%

--- Colocations.hs
+++ Colocations.hs
@@ -1,32 +1,43 @@
 -- (c) Bas van Dijk 2011 (https://github.com/basvandijk)
 
-{-# LANGUAGE OverloadedStrings #-}
-
-import Control.Monad (mapM_)
-import Data.Functor ((<$>))
+import Prelude hiding (unlines)
 import Data.Hashable (Hashable)
 import Data.Int (Int64)
-import Data.List (foldl', sort)
-import Data.Monoid (mappend)
-import System.IO (stdin)
+import Data.Monoid (mconcat)
+import Data.List (foldl', sort, intersperse)
 import qualified Data.HashMap.Strict as H
 import qualified Data.HashSet as HS
 import qualified Data.List.Split as S
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
+import qualified Data.Text              as T
+import qualified Data.Text.IO           as TIO
+import qualified Data.Text.Lazy.Builder as TLB
+import qualified Data.Text.Lazy.IO      as TLIO
 
 type Sentence   = T.Text
 type Paragraph  = [Sentence]
 type Word       = T.Text
 type Pair       = (Word, Word)
-type CountTable = H.HashMap T.Text (H.HashMap T.Text Count)
+type CountTable = H.HashMap Word (H.HashMap Word Count)
 type Count      = Int64
 
 main :: IO ()
-main = T.interact $ T.unlines . map serialize . sort . counts
+main = TIO.getContents >>=
+         TLIO.putStr . TLB.toLazyText
+                     . unlines
+                     . map serialize
+                     . sort
+                     . counts
 
-serialize :: (Word, Word, Count) -> T.Text
-serialize (w1, w2, c) = T.concat [T.pack (show c), "\t", w1, " ", w2]
+unlines :: [TLB.Builder] -> TLB.Builder
+unlines = mconcat . intersperse (TLB.singleton '\n')
+
+serialize :: (Word, Word, Count) -> TLB.Builder
+serialize (w1, w2, c) = mconcat [ TLB.fromString (show c)
+                                , TLB.singleton  '\t'
+                                , TLB.fromText   w1
+                                , TLB.singleton  ' '
+                                , TLB.fromText   w2
+                                ]
 
 counts :: T.Text -> [(Word, Word, Count)]
 counts = convert . countTable . concatMap pairs . paragraphs . T.lines
@@ -41,10 +52,10 @@ paragraphs :: [Sentence] -> [Paragraph]
 paragraphs = S.split paragraphSplitter . map T.strip
     where
       paragraphSplitter :: S.Splitter Sentence
-      paragraphSplitter = S.dropFinalBlank $ 
-                          S.dropInitBlank  $ 
-                          S.condense       $ 
-                          S.dropDelims     $ 
+      paragraphSplitter = S.dropFinalBlank $
+                          S.dropInitBlank  $
+                          S.condense       $
+                          S.dropDelims     $
                           S.whenElt T.null
 
 pairs :: Paragraph -> [Pair]
@@ -54,11 +65,11 @@ pairs sentences = helper ([]:map T.words sentences)
     helper [] = []
     helper [_] = []
     helper (x:y:[]) = sentenceCounts y [x]
-    helper (x:rs@(y:z:rest)) = sentenceCounts y [x,z] ++ helper rs
+    helper (x:rs@(y:z:_)) = sentenceCounts y [x,z] ++ helper rs
 
 sentenceCounts :: [Word] -> [[Word]] -> [Pair]
 sentenceCounts sentence context =
-    [ (w1, w2) 
+    [ (w1, w2)
     | w1 <- sentence
     , w2 <- fastNub $ concat context
     ]
